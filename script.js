45d9631ac9e0761acca2ecde039e563bfcf4dca0
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-05-10 12:22:21 +0100

    fix: adjust animation ease and clean up code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let tl = gsap.timeline();
 tl.from(".header", {
     duration: 0.5,
-    delay: 0.3,
+    delay: 0.5,
     yPercent: -100,
     opacity: 0,
     ease: "power4.out",
@@ -14,46 +14,47 @@ tl.from(".header", {
     })
     .from(
         ".hero-line2",
-        { duration: 1.5, yPercent: -100, opacity: 0, ease: "power4.out" },
+        {
+            duration: 1.5,
+            yPercent: -100,
+            opacity: 0,
+            ease: "power4.out",
+        },
         "-=1.5"
     )
     .from(
         ".hero-line3",
-        { duration: 1.5, yPercent: 100, opacity: 0, ease: "power4.out" },
+        {
+            duration: 1.5,
+            yPercent: 100,
+            opacity: 0,
+            ease: "power4.out",
+        },
         "-=1.5"
     )
     .from(
         ".hero-line4",
-        { duration: 1.5, yPercent: -100, opacity: 0, ease: "power4.out" },
+        {
+            duration: 1.5,
+            yPercent: -100,
+            opacity: 0,
+            ease: "power4.out",
+        },
         "-=1.5"
     )
     .from(
-        ".hero-intro",
-        { duration: 1.5, yPercent: -100, opacity: 0, ease: "power4.out" },
+        ".hero-intro, .hero-email, .hero-contact, .line, .hero-socials",
+        {
+            duration: 1.5,
+            yPercent: -100,
+            opacity: 0,
+            ease: "power4.out",
+            stagger: 0.375,
+        },
         "2"
     )
     .from(
         ".hero-role",
-        { duration: 1.5, yPercent: 100, opacity: 0, ease: "power4.out" },
-        "2"
-    )
-    .from(
-        ".hero-email",
-        { duration: 1.5, xPercent: -100, opacity: 0, ease: "power4.out" },
-        "2"
-    )
-    .from(
-        ".hero-contact",
-        { duration: 1.5, yPercent: -100, opacity: 0, ease: "power4.out" },
-        "2"
-    )
-    .from(
-        ".line",
-        { duration: 1.5, xPercent: 100, opacity: 0, ease: "power4.out" },
-        "2"
-    )
-    .from(
-        ".hero-socials",
-        { duration: 1.5, yPercent: 100, opacity: 0, ease: "power4.out" },
+        { duration: 2.0, yPercent: 100, opacity: 0, ease: "elastic" },
         "2"
     );
